34e03e1ae9278d48a943c9b36ee90cd9a86526ae
dua1999 <94859612+dua1999@users.noreply.github.com>
2023-04-27 15:56:06 +0400

    Delete Downloads/dist/webperf-project-1-main/webperf-project-1-main directory

--- Downloads/dist/webperf-project-1-main/webperf-project-1-main/gulpfile.js
+++
@@ -1,8 +0,0 @@
-const {src, dest} = require('gulp');
-const concat = require('gulp-concat')
-
-gulp.task('js', function(){
-    return gulp.src('js/*.js')
-    .pipe(concat('min.js'))
-    .pipe(gulp.dest('dist'))
-})
--- Downloads/dist/webperf-project-1-main/webperf-project-1-main/server.js
+++
@@ -1,9 +0,0 @@
-
-const server = createServer((req, res) => {
-  staticHandler(req, res)
-})
-
-server.listen(4000, () => {
-  console.log('Server running at http://localhost:4000/')
-})
-
